2041c75de5eb2d8ab718f4d421274a582be3009a
PythonHubStudio <example.com>
2025-02-13 23:43:48 +0400

    пагинация

--- goods/views.py
+++ goods/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
+from webbrowser import get
+from django.core.paginator import Paginator
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.template import context
 
 from goods.models import Products
 
 
-def catalog(request):
+def catalog(request, category_slug, page=1):
 
-    goods = Products.objects.all()
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, 3)
+    current_page =  paginator.page(page)
 
     context = {
         "title": "Home - Каталог",
-        "goods": goods,
+        "goods": current_page,
+        "slug_url": category_slug
     }
     return render(request, "goods/catalog.html", context)
 
