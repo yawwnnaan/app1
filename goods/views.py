41f314ce7cf0d3edecedfbfcc897b0f7c0120de8
PythonHubStudio <example.com>
2025-02-13 22:06:00 +0400

    slug converter

--- goods/views.py
+++ goods/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template import context
 
 from goods.models import Products
 
@@ -14,8 +15,15 @@ def catalog(request):
     return render(request, "goods/catalog.html", context)
 
 
-def product(request):
-    return render(request, "goods/product.html")
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, "goods/product.html", context=context)
 
 
 # Create your views here.
