5eb2d2337f57d01e36d4de2c314d82b927edaa6d
PythonHubStudio <example.com>
2025-02-16 23:56:45 +0400

    registration

--- users/views.py
+++ users/views.py
@@ -1,12 +1,11 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
-def login(request) :
-
+def login(request):
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
@@ -18,24 +17,38 @@ def login(request) :
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
-    
+
     context = {
         'title': 'Home - Авторизация',
         'form': form
     }
     return render(request, 'users/login.html', context)
 
-def registration(request) :
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+    
     context = {
-        'title': 'Home - Регистрация'
+        'title': 'Home - Регистрация',
+        'form': form
     }
     return render(request, 'users/registration.html', context)
 
-def profile(request) :
+
+def profile(request):
     context = {
         'title': 'Home - Кабинет'
     }
     return render(request, 'users/profile.html', context)
 
-def logout(request) :
-    ...
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse('main:index'))
